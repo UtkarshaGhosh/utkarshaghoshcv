7a463d0cb464eeb74c6132cb432a0ac5d9e99115
Utkarsha Ghosh <134306597+UtkarshaGhosh@users.noreply.github.com>
2025-09-06 14:13:52 +0530

    Update hero-section.tsx

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -13,7 +13,7 @@ export function HeroSection() {
     setParallax({ x: dx, y: dy })
   }
   const handleDownloadCV = async () => {
-    const CV_URL = "https://drive.google.com/file/d/1THUN_HWbFMAFGCGUlPiePq8pabfqsTCP/view?usp=sharing"
+    const CV_URL = "https://drive.google.com/uc?export=download&id=1THUN_HWbFMAFGCGUlPiePq8pabfqsTCP"
     try {
       const res = await fetch(CV_URL, { mode: "cors" })
       const blob = await res.blob()
